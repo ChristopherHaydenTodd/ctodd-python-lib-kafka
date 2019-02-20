fbc19b698de792405186713e05962e6b9083a79f
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 11:54:55 -0500

    adding library to aid in creating kafka consumers.

---
+++ kafka_helpers/kafka_consumer_helpers.py
@@ -0,0 +1,15 @@
+"""
+    Purpose:
+        Kafka General Helpers.
+
+        This library is used to aid in creating kafka consumers.
+"""
+
+# Python Library Imports
+import logging
+
+
+###
+# Consumer Helpers
+###
+
