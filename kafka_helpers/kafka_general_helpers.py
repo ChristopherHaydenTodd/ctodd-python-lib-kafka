a7cf7755dc9cfd8f9c003a605b58b40cc2fd2f2b
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 11:55:44 -0500

    adding library to aid in creating kafka producers.

---
+++ kafka_helpers/kafka_general_helpers.py
@@ -0,0 +1,16 @@
+"""
+    Purpose:
+        Kafka General Helpers.
+
+        This library is used to interact with kafka not specificlly related
+        to consuming or producing messages
+"""
+
+# Python Library Imports
+import logging
+
+
+###
+# General Helpers
+###
+
