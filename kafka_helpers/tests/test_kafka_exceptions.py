5d17f53a18e942cee84ae3f81687da52cdf70262
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 11:51:59 -0500

    Adding stub test files for the package, will be populated as package is filled

---
+++ kafka_helpers/tests/test_kafka_exceptions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Test File for kafka_exceptions.py
+"""
+
+# Python Library Imports
+import os
+import sys
+import pytest
+from unittest import mock
+
+# Import File to Test
+from kafka_helpers import kafka_exceptions
+
+
+###
+# Fixtures
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Mocked Functions
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Test Payload
+###
+
+
+# None at the Moment (Empty Test Suite)
---
+++ kafka_helpers/tests/test_kafka_producer_helpers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Test File for kafka_producer_helpers.py
+"""
+
+# Python Library Imports
+import os
+import sys
+import pytest
+from unittest import mock
+
+# Import File to Test
+from kafka_helpers import kafka_producer_helpers
+
+
+###
+# Fixtures
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Mocked Functions
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Test Payload
+###
+
+
+# None at the Moment (Empty Test Suite)
